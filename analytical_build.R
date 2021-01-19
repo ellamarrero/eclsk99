c1cb345257270c5a8a3c71c575daab5f173692a0
ellamarrero <ellamarrero@uchicago.edu>
2021-01-19 15:27:48 -0600

    Assigned NA to all missing types

--- analytical_build.R
+++ analytical_build.R
@@ -1,2 +1,29 @@
-#  analytical_build.R: builds analytical dataset (imports wide, changes necessary values in all variables, 
-# adds new/composite variables as requested), exports new wide analytical dataset
+# load libraries
+library(tidyverse)
+library(mosaic)
+library(broom)
+library(kableExtra)
+library(tidyr)
+library(tibble)
+
+# import wide dataset
+og_wide <- read.csv("/Users/ellamarrero/Dropbox/DATA CLEANING/wide/ECLSK_rebuilt_v7.csv")
+
+# change all missing types to system-missing
+# define missing types
+missing <- c(-9,-8,-7,-1,'NA')
+
+# write function to recode as system-missing if a missing type
+code_as_na<-function(x){
+  ifelse((x %in% missing),NA,x)
+}
+
+# modify original dataset
+new_wide <- og_wide %>%
+  sapply(code_as_na)
+
+new_wide <- as.data.frame(new_wide)
+
+# add new vars
+
+# export final wide analytical datset
